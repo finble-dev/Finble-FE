12a61d474fc520930b602889e639649acea8313a
chaeyeonan <dkscodus1204@ewhain.net>
2023-01-05 12:51:47 +0900

    🎨 TextRow 공통 style로 분리

--- src/assets/styles/styles.tsx
+++ src/assets/styles/styles.tsx
@@ -1,14 +1,32 @@
 import styled from 'styled-components';
 
+// Container
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: 100%;
   max-width: 1200px;
-  padding: 5rem 0;
 `;
 
+// Text
+const TextWrap = styled.div<{
+  lineHeight?: number;
+  padding?: string;
+  align?: string;
+}>`
+  padding: ${(props) => props.padding || 0};
+  line-height: ${(props) => props.lineHeight || 44.99}px;
+  text-align: ${(props) => props.align || 'left'};
+`;
+const TextRow = styled.div`
+  width: 100%;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+`;
+
+//Img
 const ImgContainer = styled.div<{ width?: string }>`
   width: ${(props) => props.width || '70%'};
   display: flex;
@@ -19,4 +37,4 @@ const Img = styled.img`
   width: 100%;
 `;
 
-export { Container, ImgContainer, Img };
+export { Container, ImgContainer, Img, TextWrap, TextRow };
