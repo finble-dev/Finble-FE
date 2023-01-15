b239bf56f880583d4ce8eaaebe43c0c124698105
Chaeyeon An <dkscodus1204@ewhain.net>
2023-01-15 22:05:41 +0900

    ✨ p.2-0 S1 Doughnut graph Done & Bar graph ing

--- src/pages/Mystock/StockDiagnosis .tsx
+++ src/pages/Mystock/StockDiagnosis .tsx
@@ -1,5 +1,22 @@
+import styled from 'styled-components';
+import Section1 from './section/Section1';
+import Section2 from './section/Section2';
+
 const StockDiagnosis = () => {
-  return <div>dvad</div>;
+  return (
+    <Container>
+      <Section1 />
+      <Section2 />
+    </Container>
+  );
 };
 
 export default StockDiagnosis;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
