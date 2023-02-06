913291e2c7e47246d8825ed027dc81e10f07805c
Chaeyeon An <dkscodus1204@ewhain.net>
2023-02-06 11:13:02 +0900

    💄 header - dropShadow 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -134,6 +134,7 @@ const Wrapper = styled.div`
   background-color: #ffffff;
   width: 100%;
   height: 50px;
+  filter: drop-shadow(0px 2px 3px rgba(0, 0, 0, 0.03));
 `;
 
 const Container = styled.div`
