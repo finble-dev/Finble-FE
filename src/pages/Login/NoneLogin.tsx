556e8ee06737053d92630e8c0bbd07ecebbdcf2f
Chaeyeon An <dkscodus1204@ewhain.net>
2023-02-06 11:40:23 +0900

    ✨ noneLogin Page 일러스트 추가

--- src/pages/Login/NoneLogin.tsx
+++ src/pages/Login/NoneLogin.tsx
@@ -2,33 +2,44 @@ import TypoGraphy from '../../components/Typography';
 import { Btn60 } from '../../components/Button';
 import styled from 'styled-components';
 import { useState } from 'react';
-import { TextWrap } from '../../assets/styles/styles';
+import { Img, ImgContainer, TextWrap } from '../../assets/styles/styles';
 import LoginModal from '../../components/LoginModal';
 import Header from '../../components/Header';
+import noneLoginIllust from '../../assets/img/noneLoginIllust.svg';
+
 const NoneLogin = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <Container>
       <Header />
-      <div style={{ marginTop: '70px' }}></div>
+      <div style={{ marginTop: '50px' }}></div>
       <Wrapper>
-        <TypoGraphy text="앗, 로그인부터!" size="h1" />
-        <TextWrap padding="13px 0 40px 0" lineHeight={29.2}>
-          <TypoGraphy
-            text="로그인/회원가입 이후 내 포트폴리오를 입력할 수 있어요."
-            size="t3"
-            color="var(--type-gray-2)"
-          />
-          <TypoGraphy
-            text="로그인하고 더 많은 정보를 확인해보세요!"
-            size="t3"
-            color="var(--type-gray-2)"
-          />
-        </TextWrap>
-        <BtnWrapper onClick={() => setModalOpen(true)}>
-          <Btn60 type="able" text="로그인하기" />
-        </BtnWrapper>
+        <div>
+          <TypoGraphy text="앗, 로그인부터!" size="h1" />
+          <TextWrap padding="13px 0 40px 0" lineHeight={29.2}>
+            <TypoGraphy
+              text="로그인/회원가입 이후 내 포트폴리오를 입력할 수 있어요."
+              size="t3"
+              color="var(--type-gray-2)"
+            />
+            <TypoGraphy
+              text="로그인하고 더 많은 정보를 확인해보세요!"
+              size="t3"
+              color="var(--type-gray-2)"
+            />
+          </TextWrap>
+          <BtnWrapper onClick={() => setModalOpen(true)}>
+            <Btn60 type="able" text="로그인하기" />
+          </BtnWrapper>
+        </div>
+        <ImgContainer
+          width="494px"
+          height="312px"
+          style={{ margin: '-50px -50px 0 0' }}
+        >
+          <Img src={noneLoginIllust} width="100%" height="100%" />
+        </ImgContainer>
       </Wrapper>
 
       {/* modal */}
@@ -48,8 +59,8 @@ const Container = styled.div`
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  width: 1204px;
+  justify-content: space-between;
+  width: 100%;
   padding-top: 249px;
   max-width: 1000px;
 `;
